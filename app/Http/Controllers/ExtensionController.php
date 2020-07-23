47137dbd47dcde2dc70203967060b5c3b6841aeb
matsukawa-s <syoudai.hand@gmail.com>
2020-07-23 18:15:38 +0900

    カレンダーの拡張機能一覧の取得の作成

--- app/Http/Controllers/ExtensionController.php
+++ app/Http/Controllers/ExtensionController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Exceptions;
 use App\Extension;
+use App\CalendarExtension;
 
 class ExtensionController extends Controller
 {
@@ -26,19 +26,32 @@ class ExtensionController extends Controller
      */
     public function exAddList(Request $request){
         //拡張機能の一覧とユーザーの現在選択しているのカレンダーの状況を加工して返したい
-        // [
-        //     {
-        //         'id' : '',
-        //         'ex_name' : '',
-        //         'explanation' : '',
-        //         'flag' : '', <- これを追加
-        //     },
-        //     {
-        //     }
-        // ]
-        $data = Extension::all();
-        $user = $request->user();
+        // $extensions = Extension::all();
+        $data = Extension::all()->toArray();
+        $calendar_id = 1;
 
+        $user = $request->user()->id;
+
+        foreach($data as $key => $value){
+            //対象のカレンダーに拡張機能を入れているかの情報を追加
+            $flag = CalendarExtension::where('calendar_id',$calendar_id)
+                ->where('extension_id',$value['id'])
+                ->first();
+
+            $data[$key]['flag'] = $flag == null ? false : true;
+        }
         return $data;
+    }
+
+    /**
+     * ユーザーの所持カレンダーに拡張機能を追加する
+     */
+    public function calendarExtensionAdd(Request $request){
+        $input = $request->all();
+
+        CalendarExtension::create([
+            'calendar_id' => $input['calendar_id'],
+            'extension_id' => $input['extension_id'],
+        ]);
     }
 }
